e885b1a8b21ef62c20bd666beb6b0f588139dbae
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2021-07-31 23:35:26 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+
+function insert(result){
+    form.displayResult.value = form.displayResult.value + result ;
+}  
+
+function equal()  
+{  
+ displayResult.value = eval(displayResult.value);
+}  
+  
+  
+function clr()  
+{  
+document.getElementById("result").value ="";
+}
