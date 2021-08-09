218835a6c1167207f8287e46b889a006162085b0
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2021-08-09 16:40:01 +0530

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,33 @@
 
-function insert(result){
-    form.displayResult.value = form.displayResult.value + result ;
-}  
 
-function equal()  
-{  
- displayResult.value = eval(displayResult.value);
-}  
-  
-  
-function clr()  
-{  
-document.getElementById("result").value ="";
+var tbcal="";
+const useinp = document.querySelector("#result");
+
+function insert(value){
+    tbcal += value;
+    useinp.value = tbcal;
+}
+
+function removedig(){
+    tbcal = tbcal.substring(0, tbcal.length - 1);
+    useinp.value = tbcal;
+}
+
+function equal(){
+    if(tbcal.length == 0 || tbcal == ""){
+        alert("Input Required!!");
+        return;
+    }
+    try{
+        ans = eval(tbcal);
+        useinp.value = ans;
+    }
+    catch(err){
+       alert("Invalid Input!!");
+    }
+}
+
+function clr(){
+    useinp.value ="";
+    tbcal="";
 }
